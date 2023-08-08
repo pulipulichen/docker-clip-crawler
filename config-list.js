96c4e6e90168b6f6a8c5d72d8d5b3fa8a8e0e3eb
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-08-08 18:43:47 +0800

    西元2023年08月08日 (週二) 18時43分47秒 CST

--- config-list.js
+++ config-list.js
@@ -23,6 +23,39 @@ let feedList = [
       ItemFiltersPreset.between3minTo30Min,
     ] // 霸道總裁攀岩開會、網紅天天搞小團體...《絕世網紅》劇情超浮誇？《 志祺今天不讀稿 》EP010｜志祺七七
   },
+  {
+    title: '斐姨所思【阿姨想知道】',
+    feedID: 'fanamericantime-interview',
+    homepageURL: 'https://www.youtube.com/channel/UC2VKL-DkRvXtWkfjMzkYvmw',
+    itemFilters: [
+      ItemFiltersPreset.between30minTo180Min,
+      (item) => { return (item.title.indexOf('斐姨所思 【阿姨想知道】') > -1) }
+    ] // 霸道總裁攀岩開會、網紅天天搞小團體...《絕世網紅》劇情超浮誇？《 志祺今天不讀稿 》EP010｜志祺七七
+  },
+  {
+    title: '動畫小坑谷',
+    feedID: 'valleylife351',
+    homepageURL: 'https://www.youtube.com/channel/UCbCb-ZUoKwQ8vcRbrH2nE0Q',
+    itemFilters: [
+      ItemFiltersPreset.between1minTo10Min,
+    ] // 霸道總裁攀岩開會、網紅天天搞小團體...《絕世網紅》劇情超浮誇？《 志祺今天不讀稿 》EP010｜志祺七七
+  },
+  {
+    title: '動畫小坑谷',
+    feedID: 'valleylife351',
+    homepageURL: 'https://www.youtube.com/channel/UCbCb-ZUoKwQ8vcRbrH2nE0Q',
+    itemFilters: [
+      ItemFiltersPreset.between1minTo10Min,
+    ] // 霸道總裁攀岩開會、網紅天天搞小團體...《絕世網紅》劇情超浮誇？《 志祺今天不讀稿 》EP010｜志祺七七
+  },
+  {
+    title: '蒼藍鴿聊醫學',
+    feedID: 'bluepigeon0810',
+    homepageURL: 'https://www.youtube.com/channel/UCUn77_F5A65HViL9OEvIpLw',
+    itemFilters: [
+      ItemFiltersPreset.between3minTo30Min,
+    ] // 霸道總裁攀岩開會、網紅天天搞小團體...《絕世網紅》劇情超浮誇？《 志祺今天不讀稿 》EP010｜志祺七七
+  },
 ]
 
 
