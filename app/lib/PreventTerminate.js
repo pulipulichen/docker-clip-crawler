254b3541baad93bd72052268050dbdb223af8946
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-08-08 20:39:23 +0800

    西元2023年08月08日 (週二) 20時39分23秒 CST

--- app/lib/PreventTerminate.js
+++ app/lib/PreventTerminate.js
@@ -14,7 +14,7 @@ module.exports = function () {
   let start = (new Date()).getTime()
   let lastUpdateTime
   let sameTimeCounter = 0
-  let maxSameTime = 2
+  let maxSameTime = 1
   setInterval(() => {
     if (lastUpdateTime !== getUpdateTime()) {
       lastUpdateTime = getUpdateTime()
