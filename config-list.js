f56fc7113ed10809b2a8c1c6e6d5d329b46f4ac1
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-08-08 20:59:07 +0800

    西元2023年08月08日 (週二) 20時59分07秒 CST

--- config-list.js
+++ config-list.js
@@ -48,6 +48,14 @@ let feedList = [
   //     ItemFiltersPreset.between3minTo30Min,
   //   ] // 霸道總裁攀岩開會、網紅天天搞小團體...《絕世網紅》劇情超浮誇？《 志祺今天不讀稿 》EP010｜志祺七七
   // },
+  {
+      title: '老阿貝聊AI繪圖',
+      feedID: 'laoabe',
+      homepageURL: 'https://www.youtube.com/channel/UCq0cd_ZAT08f3_nCL5goHcg',
+      itemFilters: [
+        ItemFiltersPreset.between3minTo30Min,
+      ] // 霸道總裁攀岩開會、網紅天天搞小團體...《絕世網紅》劇情超浮誇？《 志祺今天不讀稿 》EP010｜志祺七七
+    },
 ]
 
 
