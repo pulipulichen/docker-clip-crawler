d69b1f3948cb570d711fbeb11fdf93ce288a9e6e
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-08-26 18:31:57 +0800

    西元2023年08月26日 (週六) 18時31分57秒 CST

--- config-list.js
+++ config-list.js
@@ -3,7 +3,7 @@ const ItemFilters = require('./app/config/ItemFilters.js')
 let feedList = [
   {
     title: '原價屋coolpc on Facebook',
-    feedID: 'test-20230826-1739-coolpc-fb',
+    feedID: 'test-20230826-1831-coolpc-fb',
     feedURL: 'http://fetchrss.com/rss/5f999158644572630d2ebfc3622425f7b21ecd56f6380533.xml',
     itemFilters: [
       ItemFilters['fb'].ItemFilterFBTitle,
