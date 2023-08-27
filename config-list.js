505d32f61df947e73285320266babf9ec6ccd9c3
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-08-28 07:15:16 +0800

    西元2023年08月28日 (週一) 07時15分16秒 CST

--- config-list.js
+++ config-list.js
@@ -405,22 +405,21 @@ let feedList = [
 
 // ----------------------------------------------------------------
 
-// feedList = [
-  
-//   { // <outline type="rss" text="海芋小站++" title="海芋小站++" xmlUrl="http://pulipuli.myqnapcloud.com/304/fc/$1227-1844$/http%3A%2F%2Ffeedproxy.google.com%2Finotetw" htmlUrl="https://www.inote.tw"/>
-//     title: '奇客Solidot',
-//     feedID: 'Solidot',
-//     feedURL: 'https://www.solidot.org/index.rss',
-//     thumbnail: 'https://icon.solidot.org/images/m/logo.jpg',
-//     itemFilters: [
-//       ItemFilters['sites'].ItemFilterBlog,
-//       ItemFilters['sites'].ItemFilterSolidot
-//       // ItemFilters['common'].ItemFilterImageEmbed,
-//     ],
-//     options: {
-//       selector: '.p_mainnew'
-//     }
-//   },
-// ]
+feedList = [
+  { // <outline type="rss" text="FUNTOP資訊網++$0414-1925$" title="FUNTOP資訊網++$0414-1925$" xmlUrl="http://pulipuli.myqnapcloud.com/304/fc/$0414-1925$/http%3A%2F%2Ffuntop.tw%2Ffeed%2F" htmlUrl="https://funtop.tw/"/>
+    title: 'FUNTOP資訊網',
+    feedID: 'funtop',
+    feedURL: 'https://funtop.tw/feed/',
+    thumbnail: 'https://funtop.tw/wp-content/themes/funtop/img/favicon.ico?ver=1',
+    itemFilters: [
+      ItemFilters['sites'].ItemFilterBlog,
+      ItemFilters['sites'].ItemFilterFuntop,
+      // ItemFilters['common'].ItemFilterImageEmbed,
+    ],
+    options: {
+      selector: 'article[id]'
+    }
+  },
+]
 
 module.exports = feedList
