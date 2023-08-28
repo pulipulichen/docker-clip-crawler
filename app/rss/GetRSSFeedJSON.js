0ae4d0263fafa56d0a275d6ff1148929672644c4
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-08-29 02:43:15 +0800

    西元2023年08月29日 (週二) 02時43分15秒 CST

--- app/rss/GetRSSFeedJSON.js
+++ app/rss/GetRSSFeedJSON.js
@@ -10,7 +10,7 @@ module.exports = async function (feedURL, options = {}) {
     proxy
   } = options
 
-  return await NodeCacheSqlite.get('GetRSSFeedJSON', feedURL, async function () {
+  // return await NodeCacheSqlite.get('GetRSSFeedJSON', feedURL, async function () {
     console.log('get feed', feedURL, (new Date()).toISOString())
 
     if (!parser) {
@@ -87,5 +87,5 @@ module.exports = async function (feedURL, options = {}) {
     // console.log(output) 
 
     return output
-  }, parseInt(cacheDay * 1000 * 60 * 60 * 24, 10))
+  // }, parseInt(cacheDay * 1000 * 60 * 60 * 24, 10))
 }
