8b6a488ea73dd5f8a364566054a16b97e7a0cc6a
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-08-28 20:13:48 +0800

    西元2023年08月28日 (週一) 20時13分48秒 CST

--- config-list.js
+++ config-list.js
@@ -416,6 +416,19 @@ let feedList = [
       selector: '.article-body > .article-content > .article-content-inner'
     }
   },
+  { // <outline type="rss" text="承風雅傳HSU 電腦工作室 BLOG :: 痞客邦 PIXNET ::" title="承風雅傳HSU 電腦工作室 BLOG :: 痞客邦 PIXNET ::" xmlUrl="http://feed.pixnet.net/blog/posts/rss/hsuanthony" htmlUrl="http://hsuanthony.pixnet.net/blog"/>
+    title: '承風雅傳HSU 電腦工作室 BLOG',
+    feedID: 'hsuanthony',
+    feedURL: 'http://feed.pixnet.net/blog/posts/rss/hsuanthony',
+    itemFilters: [
+      ItemFilters['sites'].ItemFilterBlog,
+      // ItemFilters['common'].ItemFilterImageEmbed,
+      // ItemFilters['common'].ItemFilterThumbnailCache,
+    ],
+    options: {
+      selector: '.article-body > .article-content > .article-content-inner'
+    }
+  },
 ]
 
 // ----------------------------------------------------------------
