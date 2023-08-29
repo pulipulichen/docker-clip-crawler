69e291858f2f436fa9796f7dc60c6fe04d049236
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-08-30 02:56:47 +0800

    西元2023年08月30日 (週三) 02時56分47秒 CST

--- app/config/sites/ItemFilterClips.js
+++ app/config/sites/ItemFilterClips.js
@@ -63,7 +63,7 @@ let main = async function (item, options = {}) {
   let date = item.isoDate.slice(0, 7).split('-').join('/')
   let ext = 'html.docx'
   let filepath = `./output/${date}/${title}.${ext}`
-  let noteFilepath = `./output/${date}/${title}.note.html.docx`
+  let noteFilepath = `./output/${date}/${title}.note.html.docxku`
 
 
   let localpath = filepath.slice(1)
@@ -75,9 +75,9 @@ let main = async function (item, options = {}) {
       // If file size is 0, remove the file
       if (stats.size === 0) {
         fs.unlinkSync(localpath);
-        console.log('File removed successfully.');
+        // console.log('File removed successfully.');
       } else {
-        console.log('File size is not 0. No action taken.');
+        // console.log('File size is not 0. No action taken.');
         return false
       }
     } catch (err) {
