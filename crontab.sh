cee46b0fe87d7e559f8e7a0cea502c6dffc34795
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-08-29 11:45:53 +0800

    西元2023年08月29日 (週二) 11時45分53秒 CST

--- crontab.sh
+++ crontab.sh
@@ -4,3 +4,19 @@ cd "$(dirname "$0")"
 
 git pull
 npm run start
+
+file_path="./output/input.txt"
+
+# Check if the file exists
+if [ -f "$file_path" ]; then
+  # Remove the file
+  rm "$file_path"
+  echo "Removed $file_path"
+  
+  # Run the crontab.sh script
+  ./crontab.sh
+  
+else
+  echo "$file_path does not exist. Exiting."
+  exit 1
+fi
