4a628428abd584e645bf91a45638819d202b74b8
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-04-27 21:45:28 +0800

    西元2024年04月27日 (週六) 21時45分28秒 CST

--- run-singlefile.js
+++ run-singlefile.js
@@ -3,6 +3,8 @@ const https = require('https');
 const path = require('path');
 const { exec } = require('child_process');
 
+const PROXY = `https://script.google.com/macros/s/AKfycbzvvbn-FepcAJl1Ro3tgo0P4dXotOl81d4hcp4n4fNu_2RASYtO1idg6uxi0m7SXWtOMw/exec?proxy=`
+
 // Get the absolute path of the directory where the script is located
 const scriptDir = __dirname;
 
@@ -14,30 +16,56 @@ process.chdir(scriptDir);
 const inputFilePath = '/output/input.txt';
 
 if (fs.existsSync(inputFilePath) === false) {
+  console.log(`${inputFilePath} does not exists`)
   process.exit(0)
 }
 
 const HTMLtoDOCX = require('html-to-docx');
 
 function runDocker (url, outputPath) {
+  if (fs.existsSync(outputPath + '.docx')) {
+    return true
+  }
+
   return new Promise((resolve, reject) => {
-    const dockerCommand = `sudo docker run singlefile "${url}" > "${outputPath}"`;
+
+    let tmpFile = `/tmp/file.html`
+    if (fs.existsSync(tmpFile)) {
+      fs.unlinkSync(tmpFile)
+    }
+    if (fs.existsSync(tmpFile + '.docx')) {
+      fs.unlinkSync(tmpFile + '.docx')
+    } 
+
+    const dockerCommand = `docker run singlefile "${url}" > "${outputPath}"`;
+    const dockerCommandProxy = `sudo docker run singlefile "${url}" > "${tmpFile}"`;
     console.log(`[RUN] ${dockerCommand}`)
-    exec(dockerCommand, async (error) => {
+    exec(dockerCommandProxy, async (error) => {
       if (error) {
         console.error('Error running Docker command:', error);
         reject(error)
       } else {
-        console.log(`Downloaded ${url} and saved to ${outputPath}`);
+        console.log(`Downloaded ${url} and saved to ${tmpFile}`);
 
-        let htmlString = await fs.readFileSync(outputPath, 'utf8')
-        const fileBuffer = await HTMLtoDOCX(htmlString, null, {
-          table: { row: { cantSplit: true } },
-          footer: true,
-          pageNumber: true,
-        });
-      
-        fs.writeFileSync(outputPath + '.docx', fileBuffer)
+        if (!fs.existsSync(outputPath)) {
+          fs.copyFileSync(tmpFile, outputPath)
+        }
+
+        if (fs.existsSync(outputPath + '.docx') === false) {
+          let htmlString = await fs.readFileSync(tmpFile, 'utf8')
+          const fileBuffer = await HTMLtoDOCX(htmlString, null, {
+            table: { row: { cantSplit: true } },
+            footer: true,
+            pageNumber: true,
+          });
+        
+          fs.writeFileSync(tmpFile + '.docx', fileBuffer)
+          fs.copyFileSync(tmpFile + '.docx', outputPath + '.docx')
+        }
+          
+
+        
+        
 
         resolve(true)
       }
@@ -51,6 +79,8 @@ fs.readFile(inputFilePath, 'utf8', async (err, data) => {
     console.error('Error reading input file:', err);
     return;
   }
+
+  console.log(data)
 
   const lines = data.trim().split('\n');
 
