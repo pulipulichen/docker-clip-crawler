936df65322eba640394c027debe400d472aeffda
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-04-27 21:23:05 +0800

    西元2024年04月27日 (週六) 21時23分05秒 CST

---
+++ init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install Docker
+sudo apt-get update
+sudo apt-get install -y docker.io
+
+# Install Node.js
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Verify installations
+docker --version
+node --version
+
+sudo apt-get install davfs2 -y
+echo "use_locks 0" | sudo tee -a /etc/davfs2/davfs2.conf
+# echo "${WEBDAV_URL} ${WEBDAV_USER} ${WEBDAV_PW}" | sudo tee /etc/davfs2/secrets
+# sudo chmod 600 /etc/davfs2/secrets
+
+# sudo mkdir /output
