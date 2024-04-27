bf685ab9e46adfa7c1beaf2f8b0b65b1f925863f
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-04-27 22:48:13 +0800

    西元2024年04月27日 (週六) 22時48分13秒 CST

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y docker.io
 
 # Install Node.js
 curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
-sudo apt-get install -y nodejs npm
+sudo apt-get install -y nodejs
 
 # Verify installations
 docker --version
@@ -23,4 +23,9 @@ sudo docker pull capsulecode/singlefile
 sudo docker tag capsulecode/singlefile singlefile
 
 cd $(dirname $0)
+sudo apt-get install -y npm
 npm i
+
+mkdir -p /output
+
+# <WebDAV_URL> /output davfs rw,user,noauto 0 0
