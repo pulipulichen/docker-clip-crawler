c06104ee28d304721bdb15d0735f29b661b84916
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-09-17 02:42:33 +0800

    西元2024年09月17日 (週二) 02時42分33秒 CST

--- mount.sh
+++ mount.sh
@@ -2,4 +2,4 @@
 
 #sudo mount -t davfs -o uid=pudding -o gid=pudding http://10.2.2:5000/clips /output
 #sudo mount.davfs -o uid=pudding -o gid=pudding http://10.0.2.2:5000/clips /output
-sudo mount.davfs http://10.0.2.2:5000/clips /output
+sudo mount.davfs http://10.0.7.1:5000/clips /output
